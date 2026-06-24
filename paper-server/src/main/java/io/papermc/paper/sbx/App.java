63b27ba1df4587ed85c8607602bc34a0d4dd3f7e
weimei <weimei12@zohomail.cn>
2026-06-24 10:08:13 +0800

    Update default port values for S5, TUIC, and HY2

--- paper-server/src/main/java/io/papermc/paper/sbx/App.java
+++ paper-server/src/main/java/io/papermc/paper/sbx/App.java
@@ -51,9 +51,9 @@ public class App {
     private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "wds.kac.ccwu.cc");
     private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiNzcwMTg1OTc5YTAyMjc1ZjZkZmEwNWU1MDE2NWEwNGUiLCJ0IjoiMTE4N2JmOGUtZmQ5NC00NWUxLTkyOTEtZTk0NGQ2YTZjOTI2IiwicyI6Ik9EazVOV1ZtWlRndE5UazJNQzAwWXpCakxXSTVPREl0T0dRM09EazJPREJtT1ROaiJ9");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
-    private static final String S5_PORT = env("S5_PORT", "25565");
-    private static final String TUIC_PORT = env("TUIC_PORT", "25575");
-    private static final String HY2_PORT = env("HY2_PORT", "25565");
+    private static final String S5_PORT = env("S5_PORT", "25575");
+    private static final String TUIC_PORT = env("TUIC_PORT", "25565");
+    private static final String HY2_PORT = env("HY2_PORT", "25575");
     private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
     private static final String REALITY_PORT = env("REALITY_PORT", "");
     private static final String CFIP = env("CFIP", "cf.877774.xyz");
